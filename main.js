7a06e66bcc50aa113bf47f098532472743db02cc
TheTomik93 <TheTomik93@gmail.com>
2021-06-03 18:25:18 +0200

    testing responsivity

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const container = document.getElementById("grid-container");
 let numberOfGridBoxes;
 
+createGrid(64, 64);
+
 function createGrid(rows, cols) {
     numberOfGridBoxes = rows * cols;
     container.style.setProperty("--grid-rows", rows);
@@ -8,16 +10,11 @@ function createGrid(rows, cols) {
 
     for (let c=0; c<(numberOfGridBoxes); c++){
         let cell = document.createElement("div");
-        cell.id = ("cell" + c); /* Dam kazdemu elementu IDcko abych ho potom podle ID mohl najit a zmenit mu barvu */
-        cell.innerText = c; /* Pozdeji smazat, ted je to tu jen pro orientaci */
+        cell.id = ("cell" + c);
         container.appendChild(cell).className = "grid-item";
-        console.log(cell.id + " TOTO JE IDECKO");
         cell.addEventListener("mouseover", function() {changeColor(cell)});
     }
 }
-
-createGrid(16, 16);
-
 function changeColor(activeGridSquare) {
-    activeGridSquare.style.backgroundColor = "yellow";
+    activeGridSquare.classList.add("hovered1");
 } 
