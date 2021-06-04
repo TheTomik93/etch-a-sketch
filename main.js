a3bce9f7b5da9e0e33539c4906a076ea7ff3d632
TheTomik93 <TheTomik93@gmail.com>
2021-06-04 17:34:31 +0200

    fixed: ClearAll button not working properly added: highlighting of the active button

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 const container = document.getElementById("grid-container");
 const clearButton = document.getElementById("btnClear");
 const sizeButton = document.getElementById("btnSize");
-const grascaleButton = document.getElementById("btnGrayscale");
+const grayscaleButton = document.getElementById("btnGrayscale");
 const rainbowButton = document.getElementById("btnRainbow");
 
-let numberOfGridBoxes;
 let size = 16; /*Default size of the grid*/
+let numberOfGridBoxes = size * size;
 let colorScheme = 1; /*Defaultne grayscale schema*/
 
 clearButton.addEventListener("click", clearGrid);
 sizeButton.addEventListener("click", askSize);
-grascaleButton.addEventListener("click", setGrayscale); /* */
+grayscaleButton.addEventListener("click", setGrayscale); /* */
 rainbowButton.addEventListener("click", setRainbow);
 
 createGrid(size, size);
@@ -23,7 +23,8 @@ function createGrid(rows, cols) {
 
     for (let c = 0; c < numberOfGridBoxes; c++){
         let cell = document.createElement("div");
-        cell.id = ("cell" + c);
+        cell.id = c;
+        /*cell.textContent = cell.id;*/
         container.appendChild(cell).className = "grid-item";
         cell.addEventListener("mouseover", function() {changeColor(cell)});
     }
@@ -40,26 +41,38 @@ function changeColor(activeGridSquare) {
 
 function clearGrid(){
     for (let i = 0; i < numberOfGridBoxes; i++){
-        let cellToClear = document.getElementById("cell" + i);
-        cellToClear.className = "grid-item";
+        let cellToClear = document.getElementById(i);
+        container.removeChild(cellToClear);
     }
+    createGrid(size, size);
 }
 
 function askSize(){
-    size = prompt("Enter the length of the row / column:", 16);
-    if (isNaN(size)){
+    desiredSize = prompt("Enter the length of the row / column:", 16);
+    if (isNaN(desiredSize)){
         alert("Only numbers accepted");
     }
+    else if(desiredSize == 0){
+        alert("Minimal size is 1");
+    }
     else{
+        size = desiredSize;
         clearGrid();
-        createGrid(size, size);
     }
 }
 
 function setGrayscale(){
     colorScheme = 1;
+    grayscaleButton.style.setProperty("border", "3px solid red");
+    grayscaleButton.style.setProperty("box-shadow", "2px 2px 5px red");
+    rainbowButton.style.setProperty("border", "0px solid red");
+    rainbowButton.style.setProperty("box-shadow", "0px 0px 0px red");
 }
 
 function setRainbow() {
     colorScheme = 2;
+    grayscaleButton.style.setProperty("border", "0px solid red");
+    grayscaleButton.style.setProperty("box-shadow", "0px 0px 0px red");
+    rainbowButton.style.setProperty("border", "3px solid red");
+    rainbowButton.style.setProperty("box-shadow", "2px 2px 5px red");
 }
